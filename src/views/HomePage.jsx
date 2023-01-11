eb3f576a445b3dd5629d5c9dd337fd2b628ddce6
JKcml <j.kresi@yahoo.co.uk>
2023-01-11 11:41:02 +0000

    homepage nav links added

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -6,6 +6,11 @@ const Homepage = () => {
     return (
     <Container id="homepage">
         <h2>homepage</h2>
+        <nav>
+            <a href="https://regal-cobbler-3c979e.netlify.app/Login">Log in</a><br></br>
+            <a href="https://regal-cobbler-3c979e.netlify.app/registerUser">Register</a><br></br>
+            <a href="https://regal-cobbler-3c979e.netlify.app/searchRecipes">Search recipe</a>
+        </nav>
     </Container>
     );
 };
