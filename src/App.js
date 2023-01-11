5894fe2a28b1713fe3e0ff9751df6fecdce60f8f
Chadrine-Azimendem <chadrine.azimendem@gmail.com>
2023-01-11 18:14:17 +0000

    login page done

--- src/App.js
+++ src/App.js
@@ -1,79 +1,60 @@
-import './css/common.css';
+import "./css/common.css";
 import React from "react";
+import { useState } from "react";
 //import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Homepage from "./views/HomePage"
-import LoginPage from "./views/LoginPage"
-import PageNotFound from './views/PageNotFound';
-import RecipeInfoPage from './views/RecipeInfoPage';
-import RecipeSearchPage from './views/RecipeSearchPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "./views/HomePage";
+import LoginPage from "./views/LoginPage";
+import PageNotFound from "./views/PageNotFound";
+import RecipeInfoPage from "./views/RecipeInfoPage";
+import RecipeSearchPage from "./views/RecipeSearchPage";
 import RegisterPage from "./views/RegisterPage";
-import UserProfilePage from "./views/UserProfilePage"
-import Navbar from "./components/Navbar"
+import UserProfilePage from "./views/UserProfilePage";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    
+	const [jwt, setJWT] = useState();
+	const [loggedInUser, setLoggedInUser] = useState();
 
-  
-    <BrowserRouter >
-    <Navbar />
-    <Routes>
-      <Route
-        path="/"
-        element={
-          <Homepage />
-        }
-      />
+	return (
+		<BrowserRouter>
+			<Navbar />
+			<br />
+			<br />
+			<br />
+			{jwt ? <p>{jwt}</p> : null}
+			{loggedInUser ? <p>{loggedInUser.username}</p> : null}
+			<Routes>
+				<Route path="/" element={<Homepage />} />
 
-      <Route
-        path="/searchRecipes"
-        element={
-          <RecipeSearchPage />
-        }
-      />
-      <Route
-        path="/viewRecipie"
-        element={
-          <RecipeInfoPage />
-        }
-      />
-      <Route
-        path="/registerUser"
-        element={
-          <RegisterPage />
-        }
-      />
-      <Route
-        path="/UserProfile"
-        element={
-          <UserProfilePage />
-        }
-      />
-      <Route
-        path="/login"
-        element={
-          <LoginPage action="login" />
-        }
-      />
-      <Route
-        path="/logout"
-        element={
-          <LoginPage action="logout" />
-        }
-      />
-      <Route
-        path="*"
-        element={
-        <PageNotFound />
-        }
-      />
-    </Routes>
-    
-    </BrowserRouter>
-   
-
-  );
+				<Route path="/searchRecipes" element={<RecipeSearchPage />} />
+				<Route path="/viewRecipie" element={<RecipeInfoPage />} />
+				<Route path="/registerUser" element={<RegisterPage />} />
+				<Route path="/UserProfile" element={<UserProfilePage />} />
+				<Route
+					path="/login"
+					element={
+						<LoginPage
+							setJWT={setJWT}
+							setter={setLoggedInUser}
+							action="login"
+						/>
+					}
+				/>
+				<Route
+					path="/logout"
+					element={
+						<LoginPage
+							setJWT={setJWT}
+							setter={setLoggedInUser}
+							action="logout"
+						/>
+					}
+				/>
+				<Route path="*" element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
