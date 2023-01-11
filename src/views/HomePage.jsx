e9d307ee457f09cb7095a9a91cbc4d30c5d463cf
JKcml <j.kresi@yahoo.co.uk>
2023-01-11 18:00:46 +0000

    homepage layout

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,15 +1,23 @@
 import { Container } from "../css/common-styles"
-
+import "../css/homepage.css"
 const Homepage = () => {
 
 
     return (
     <Container id="homepage">
-        <h2>homepage</h2>
-        <nav>
-            <a href="https://regal-cobbler-3c979e.netlify.app/Login">Log in</a><br></br>
-            <a href="https://regal-cobbler-3c979e.netlify.app/registerUser">Register</a><br></br>
-            <a href="https://regal-cobbler-3c979e.netlify.app/searchRecipes">Search recipe</a>
+        <h2 class="">homepage</h2>
+        <nav class="navbar">
+            <div class="outer">
+                <div class="btn-group">
+                    
+                    <button class="btn-lg"><a class="homepageLink" href="https://regal-cobbler-3c979e.netlify.app/Login">Log in</a></button>
+                    <button class="btn-lg"><a class="homepageLink" href="https://regal-cobbler-3c979e.netlify.app/registerUser">Register</a></button>
+                
+                </div>
+                <div class="inner">
+                    <button class="btn-lg"><a class="homepageLink" href="https://regal-cobbler-3c979e.netlify.app/searchRecipes">Search recipe</a></button>
+                </div>
+            </div>
         </nav>
     </Container>
     );
