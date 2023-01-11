96311aeef0973624084cff9e31bb8263579b579b
Dave Hale <davejlhale@yahoo.co.uk>
2023-01-11 15:02:54 +0000

    gallery navigation to recipeinfopage

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
       <Route
         path="/viewRecipie"
         element={
-          <RecipeInfoPage recipe={recipe}/>
+          <RecipeInfoPage data={recipe}/>
         }
       />
       <Route
