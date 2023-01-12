255b3ca5b904673dc7b175cc87d8270735b62b6b
Dave Hale <davejlhale@yahoo.co.uk>
2023-01-12 23:56:28 +0000

    footer height to try pop api logo on chrome

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -10,6 +10,7 @@ export const FooterDiv = styled.nav`
     z-index: 10;
     width: 100%; /* Full width */
     color:#fff;
+    height:45px;
     box-shadow: rgba(0, 0, 0, 0.29) 0px 10px 20px, rgba(0, 0, 0, 0.33) 0px 8px 8px, rgba(0, 0, 0, 0.53) 0px 3px 3px;
 `
 
