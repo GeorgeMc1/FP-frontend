ee14abfc183a70f316b32657f346a4931857152f
Dave Hale <davejlhale@yahoo.co.uk>
2023-01-12 00:24:53 +0000

    footer component added

---
+++ src/components/Footer/FooterElements.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+
+export const FooterDiv = styled.nav`
+    background: #000; 
+    display: flex;
+    justify-content: space-around;
+    position: fixed;
+    bottom:0;
+    z-index: 10;
+    width: 100%; /* Full width */
+    color:#fff;
+    box-shadow: rgba(0, 0, 0, 0.29) 0px 10px 20px, rgba(0, 0, 0, 0.33) 0px 8px 8px, rgba(0, 0, 0, 0.53) 0px 3px 3px;
+`
+
+
+
+
