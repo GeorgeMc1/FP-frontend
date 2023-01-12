26e5e24b109f2f5d23bb74ebfdebe4a8e6af5da5
Dave Hale <davejlhale@yahoo.co.uk>
2023-01-12 22:08:00 +0000

    edamam attribution added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ import { FooterDiv } from './FooterElements';
 const Footer = () => {
   return (
     <FooterDiv>
-    <p>&copy;2023 footer fixed by css for now </p>
+    <p>&copy;2023 footer fixed by css for now </p><div id="edamam-badge" data-color="transparant"></div>
     </FooterDiv>
 
 
