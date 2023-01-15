af023f66e8c73cd40fb3d3f110297a9a7877d533
Dave Hale <davejlhale@yahoo.co.uk>
2023-01-15 15:39:07 +0000

    block demo

--- src/pages/HomePageScaff.jsx
+++ src/pages/HomePageScaff.jsx
@@ -3,17 +3,33 @@ import "../css/homepage.css"
 import { PageContainer } from "../css/common-styles"
 
 import homepage from "../assets/images/Home-Page_scaf.png"
+import { useState } from "react";
 const HomepageScaff = () => {
-
+    const[togStatus,settogstatus] = useState(1);
+    const ontoggle = () => {
+        
+      console.log(togStatus)  
+        if (togStatus === 2) {
+            settogstatus( 1)
+        }else {
+            settogstatus(togStatus+1)
+        }
+        return
+    }
 
     return (
         <PageContainer id="homepage">
 
-
+     
+{/* add nothing above here */}      
+{togStatus===1 ? 
             <div>
                 <img className="temp" alt="temp" src={homepage} width="100%" position="absolute" top="0" />
             </div>
+            :
+            null}
 
+{togStatus===2 ?
             <div id="demo">
 
                 <div className="level1">
@@ -47,13 +63,13 @@ const HomepageScaff = () => {
                         </div>
                     </div>
                     <div  >
-                        <img alt=""/>
+                        <img alt="" />
                         <div >
                             text
                         </div>
                     </div>
                     <div  >
-                        <img alt=""/>
+                        <img alt="" />
                         <div >
                             text
                         </div>
@@ -66,7 +82,7 @@ const HomepageScaff = () => {
 
                 <div className="level1">
                     <div >
-                        <img alt=""/>
+                        <img alt="" />
                         <p> text</p>
                         <button />
                     </div>
@@ -82,11 +98,11 @@ const HomepageScaff = () => {
                     <div >
                         <p> </p>
                         <div>
-                            <div social icons>
-                                <img alt=""/>
-                                <img alt=""/>
-                                <img alt=""/>
-                                <img alt=""/>
+                            <div >
+                                <img alt="" />
+                                <img alt="" />
+                                <img alt="" />
+                                <img alt="" />
                             </div>
                         </div>
                         <div  >
@@ -94,15 +110,18 @@ const HomepageScaff = () => {
                                 <p>text</p>
                             </div>
                             <div >
-                                <img alt=""/>
+                                <img alt="" />
                             </div>
                         </div>
                     </div>
                 </div>
 
             </div>
+  :
+  null}
 
-
+{/* add nothing below here */}
+<button onClick={ontoggle} >toggle</button>
         </PageContainer>
 
     );
