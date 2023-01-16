1454cf7f5dff64228a642741db07f2ab75d91852
GeorgeMc1 <george.man1@hotmail.co.uk>
2023-01-16 09:50:43 +0000

    added notes section and added some styling

--- src/components/RecipeInfoPage/InfoBottomSec.jsx
+++ src/components/RecipeInfoPage/InfoBottomSec.jsx
@@ -6,18 +6,22 @@ import styled from "styled-components";
 const InfoBottomSec = ({ingLines, cautions, digest}) => {
     const [ingredients, setIngredients] = useState(true);
     const [nutrition, setNutrition] = useState(false);
+    const [notes, setNotes] = useState(false);
 
     return(
         <BottomSecContainer>
             <div className="tabs">
-                <h3 onClick={() => {setIngredients(true); setNutrition(false)}}>Ingredients</h3>
-                <h3 onClick={() => {setNutrition(true); setIngredients(false)}}>Nutritinal Info</h3>
+                <h3 className={ingredients ? "selected" : ""} onClick={() => {setIngredients(true); setNutrition(false); setNotes(false)}}>Ingredients</h3>
+                <h3 className={nutrition ? "selected" : ""} onClick={() => {setNutrition(true); setIngredients(false); setNotes(false)}}>Nutritinal Info</h3>
+                <h3 className={notes ? "selected" : ""} onClick={() => {setNotes(true); setIngredients(false); setNutrition(false)}}>Notes</h3>
             </div>
-            <div>
+            <div className="info selected">
                 {ingredients ?
                     <IngredientList data={ingLines} cautions={cautions}/>
                 : nutrition ?
                     <NutritionalList data={digest}/>
+                : notes ?
+                    <textarea className="input"></textarea>
                 : null
                 }
             </div>
@@ -29,13 +33,14 @@ const BottomSecContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    max-width: 1000px;
+    max-width: 800px;
+    width: 100%;
     margin: 10px auto;
 
     .tabs{
         display: flex;
         h3{
-            margin: 0 10px;
+            padding: 0 10px;
             cursor: pointer;
             :hover{
                 text-decoration: underline;
@@ -44,6 +49,18 @@ const BottomSecContainer = styled.div`
     }
     h3{
         font-family: Poppins, sans-serif;
+    }
+    .selected{
+        background-color: white;
+    }
+    .info{
+        width: 100%;
+        .input{
+            margin: 5px;
+            width: calc(100% - 10px);
+            min-height: 200px;
+            resize: none;
+        }
     }
 `
 export default InfoBottomSec;
