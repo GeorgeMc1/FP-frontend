350623218ce3368d675f2974004d5c4f696d62ce
GeorgeMc1 <george.man1@hotmail.co.uk>
2023-01-16 16:30:50 +0000

    login page styling

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,11 +3,9 @@ import { NavLink as Link } from "react-router-dom";
 
 
 export const Nav = styled.nav`
-    background: #000; 
+    background: #000;
     display: flex;
     justify-content: space-around;
-    position: fixed;
-    top:0;
     z-index: 10;
     width: 100%; /* Full width */
     box-shadow: rgba(0, 0, 0, 0.29) 0px 10px 20px, rgba(0, 0, 0, 0.33) 0px 8px 8px, rgba(0, 0, 0, 0.53) 0px 3px 3px;
@@ -41,14 +39,12 @@ export const NavIconLink = styled(Link)`
         width:120px;
         height:60px;
         vertical-align: middle; 
-        height:100%
-        transition: all 0.3s ease-in-out;
+        height:100%;
         transition: all 0.3s ease-in-out;
         display: inline-block;
         
         &:hover {
-     
-    transform: scale(1.4);
+            transform: scale(1.4);
         }
     }
 
