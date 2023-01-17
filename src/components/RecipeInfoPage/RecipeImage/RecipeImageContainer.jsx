63cec89ca744a85fb58ea49d4f9a984dfcf45b87
GeorgeMc1 <george.man1@hotmail.co.uk>
2023-01-17 14:41:54 +0000

    changed margin on image

--- src/components/RecipeInfoPage/RecipeImage/RecipeImageContainer.jsx
+++ src/components/RecipeInfoPage/RecipeImage/RecipeImageContainer.jsx
@@ -4,12 +4,13 @@ const RecipeImageContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    padding-top: 5px;
+    /* padding-top: 5px; */
     background-color: rgb(194, 251, 215);
     border-radius: 0 0 10px 10px;
     img{
         border-radius: 10px;
         margin: 5px;
+        margin-top: 0;
         border: 2px solid rgb(0, 0, 0, 0.2);
     }
 `
