c7314ec076e24bc2dce001f78583a4fd54536f32
GeorgeMc1 <george.man1@hotmail.co.uk>
2023-01-17 10:33:32 +0000

    small login in page changes and some changes to the info page

--- src/components/RecipeInfoPage/InfoContainer.jsx
+++ src/components/RecipeInfoPage/InfoContainer.jsx
@@ -3,23 +3,25 @@ import styled from "styled-components";
 const InfoContainer = styled.div`
     display: flex;
     flex-direction: column;
-    /* align-items: center; */
     max-width: 800px;
-    /* margin: 0 auto; */
 
     h2{
         text-align: center;
     }
     ul{
         margin: 5px 0;
+        padding: 0 0 0 25px;
     }
     #nutrition{
         display: flex;
         justify-content: center;
     }
     &.recipeAction{
+        padding: 10px;
         margin-left: 15px;
         align-items: center;
+        border-radius: 5px;
+        border: 10px solid rgb(194, 251, 215);
     }
     .recipeLink{
         color: black;
