8fe5f3fdf5e9ecc75b7dd345e3ceed4c1448038e
GeorgeMc1 <george.man1@hotmail.co.uk>
2023-01-19 13:17:02 +0000

    footer fixes

--- src/components/PreFooter/PreFooter.styles.js
+++ src/components/PreFooter/PreFooter.styles.js
@@ -1,43 +1,42 @@
 import styled from 'styled-components';
 import dash from "../../assets/images/border.png"
 export const PreFooterContainer = styled.div`
-display:flex;
-flex-direction: column;
-margin-top:30px;
-background-color:#ddF9cf;
-padding-top10px;
-box-shadow: rgba(0, 0, 0, 0.29) 0px 10px 20px, rgba(0, 0, 0, 0.33) 0px 8px 8px, rgba(0, 0, 0, 0.53) 0px 3px 3px;
-#column { 
     display:flex;
-    flex-direction:row;
-    width:100%;
-    margin:auto ;
-    align-items: center;
+    flex-direction: column;
+    margin-top:30px;
+    background-color:#ddF9cf;
+    padding-top: 10px;
     box-shadow: rgba(0, 0, 0, 0.45) 0px -5px 5px -5px;
 
+    #column {
+        display:flex;
+        flex-direction:row;
+        width:100%;
+        margin:auto ;
+        align-items: center;
 
-@media screen and (max-width: 600px) {
-    flex-direction:column;
-  }
-}
-    
+        @media screen and (max-width: 600px) {
+            flex-direction:column;
+        }
+    }
 `
 
 export const PreFooterHalfColumnContainer = styled.div`
-display:flex;
-flex-direction: column;
+    display:flex;
+    flex-direction: column;
 
-// justify-self: center;
- align-self:center;
- justify-items: center;
-align-items: center;
-flex: 1 1 50%;
-    button{
+    // justify-self: center;
+    align-self:center;
+    justify-items: center;
+    align-items: center;
+    flex: 1 1 50%;
+    .button{
         margin:10px; 
         padding:10px 40px;
         border:1px solid black;
         background-color:#454545;
         color:#ddF9cf;
+        text-decoration: none;
     }
     img{
         background-color: none;
@@ -52,18 +51,17 @@ flex: 1 1 50%;
 `
 
 export const StrapLine = styled.div`
-border-top: 5px solid transparent;
+    border-top: 5px solid transparent;
     border-image-source: url(${dash});
     border-image-repeat: round;
     border-image-slice: 30 ;
     width:100%;
     margin:auto;
 
-#borderimg3 { 
-    border: 15px solid transparent;
-    border-image-source: url(${dash});
-    border-image-repeat: round;
-    border-image-slice: 30;
-  }
-}
+    #borderimg3 {
+        border: 15px solid transparent;
+        border-image-source: url(${dash});
+        border-image-repeat: round;
+        border-image-slice: 30;
+    }
 `
